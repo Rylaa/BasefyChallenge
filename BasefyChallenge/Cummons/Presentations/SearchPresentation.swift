b2419c3b8da0bc7a09cbf7b53ff119012b7feba3
yusuf demirkoparan <ydemirkoparan@gmail.com>
2019-05-22 12:56:42 +0300

    Done

--- BasefyChallenge/Cummons/Presentations/SearchPresentation.swift
+++ BasefyChallenge/Cummons/Presentations/SearchPresentation.swift
@@ -5,9 +5,7 @@
 //  Created by Yusuf DEMİRKOPARAN on 21.05.2019.
 //  Copyright © 2019 Yusuf DEMİRKOPARAN. All rights reserved.
 //
-
 import Foundation
-
 struct SearchPresentation : Equatable {
     let repoName       : String
     let forks          : Int
--- BasefyChallenge/Cummons/Presentations/UserProfilePresentation.swift
+++ BasefyChallenge/Cummons/Presentations/UserProfilePresentation.swift
@@ -5,9 +5,7 @@
 //  Created by Yusuf DEMİRKOPARAN on 21.05.2019.
 //  Copyright © 2019 Yusuf DEMİRKOPARAN. All rights reserved.
 //
-
 import Foundation
-
 struct UserProfilePresentation : Equatable {
     var userName       : String
     var userProfilImg  : String
