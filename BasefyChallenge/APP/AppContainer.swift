dbb03bdb8cf50253bbbe5bcd6e30730d9d35d7be
yusuf demirkoparan <ydemirkoparan@gmail.com>
2019-05-22 12:55:48 +0300

    done

--- BasefyChallenge/APP/AppContainer.swift
+++ BasefyChallenge/APP/AppContainer.swift
@@ -6,7 +6,6 @@
 //  Created by Yusuf DEMİRKOPARAN on 21.05.2019.
 //  Copyright © 2019 Yusuf DEMİRKOPARAN. All rights reserved.
 //
-
 import Foundation
 let app = AppContainer()
 final class AppContainer {
